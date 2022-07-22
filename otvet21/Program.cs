3667b23411f262a761fd9d588d91b0e84bbf1257
Elena <elena.tim@bk.ru>
2022-07-22 11:03:11 +0300

    задача21

---
+++ otvet21/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+var result =Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+
+Console.WriteLine(result);
